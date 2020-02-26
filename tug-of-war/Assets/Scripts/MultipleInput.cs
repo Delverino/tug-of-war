fca064766334e389a3a0b96d48a68c3e7da94dfc
Ezra Szanton <ezraszanton@yahoo.com>
2020-02-26 01:40:25 -0500

    input detection

--- tug-of-war/Assets/Scripts/MultipleInput.cs
+++ tug-of-war/Assets/Scripts/MultipleInput.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class MultipleInput : MonoBehaviour
 {
+    [System.Serializable]
+    public class Controls
+    {
+        public KeyCode up;
+        public KeyCode down;
+        public KeyCode left;
+        public KeyCode right;
+    }
+
+    public Controls player_1;
+    public Controls player_2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,14 @@ public class MultipleInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(get_direction(player_1) + get_direction(player_2));
+    }
+
+    Vector2 get_direction(Controls c)
+    {
+        int x = System.Convert.ToInt32(Input.GetKey(c.right)) - System.Convert.ToInt32(Input.GetKey(c.left));
+        int y = System.Convert.ToInt32(Input.GetKey(c.up)) - System.Convert.ToInt32(Input.GetKey(c.down));
         
+        return new Vector2(x, y);
     }
 }
